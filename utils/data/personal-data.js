9dd76ab6470e17d8b7241ee8055dccd7a510a204
Anuj <anuj8ranchi@gmail.com>
2025-03-29 15:49:04 +0530

    updated the personal-data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Anuj Verma",
   profile: '/profile.jpg',
   designation: "Software Developer",
-  description: "I'm Anuj Verma, a professional software developer with a proven track record of designing and building high-quality web applications. I thrive on quickly learning and proactively exploring new technologies to solve real-world challenges. I prioritize industry best practices, ensuring that my applications are secure, efficient, and reliable. Collaboration is key for me—I enjoy working closely with cross-functional teams to achieve business goals and create impactful solutions. My expertise lies primarily in the JavaScript tech stack, and I'm available for the opportunities that align with my skills and interests.",
+  description: "I'm Anuj Verma, a professional software developer with a proven track record of designing and building high-quality web applications. I thrive on quickly learning and proactively exploring new technologies to solve real-world challenges. I prioritize industry best practices, ensuring that my applications are secure, efficient, and reliable. Collaboration is key for me, I enjoy working closely with cross-functional teams to achieve business goals and create impactful solutions. My expertise lies primarily in the JavaScript tech stack, and I'm available for the opportunities that align with my skills and interests.",
   email: 'anuj8rancha@gmail.com',
   phone: '+917061125133',
   address: 'Chatra(Simaraia), Jharkhand, India - 825103 ',
